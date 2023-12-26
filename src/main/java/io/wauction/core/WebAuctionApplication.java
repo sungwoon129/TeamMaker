0176b3c8f06f2cb7b9ed1bd1782d15c60f05eb16
master <sungwoony@kakao.com>
2023-12-26 22:47:21 +0900

    Initial commit

---
+++ src/main/java/io/wauction/core/WebAuctionApplication.java
@@ -0,0 +1,13 @@
+package io.wauction.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAuctionApplication.class, args);
+    }
+
+}
