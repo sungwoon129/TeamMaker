96019b808b446e37c7142d056e079fbfc0c40fc6
master <sungwoony@kakao.com>
2024-02-05 23:58:56 +0900

    feat: 클라이언트 웹소켓 연결 및 화면 코드

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,57 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/wauction');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (msg) {
+            console.log(msg);
+            showMessage(JSON.parse(msg.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+    const data = {
+        channelId: 1,
+        senderId: "경매참가자1",
+        type:"bid",
+        message: "1000"
+    }
+    stompClient.send("/app/hello", {}, JSON.stringify(data));
+}
+
+function showMessage(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendMessage(); });
+});
