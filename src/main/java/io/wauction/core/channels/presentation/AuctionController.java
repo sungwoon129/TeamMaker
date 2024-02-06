0455d3c55836e23d54238145019f66e33b756402
master <sungwoony@kakao.com>
2024-02-06 23:59:04 +0900

    refactor: 불필요 클래스.의존성.엔티티 제거(Member)
    - 비회원으로 운영할 계획이라 Member 엔티티 삭제

--- src/main/java/io/wauction/core/channels/presentation/AuctionController.java
+++ src/main/java/io/wauction/core/channels/presentation/AuctionController.java
@@ -1,15 +1,11 @@
 package io.wauction.core.channels.presentation;
 
-import io.wauction.core.channels.application.BidService;
-import io.wauction.core.channels.dto.AuctionRequest;
 import io.wauction.core.channels.dto.MessageRequest;
 import io.wauction.core.channels.dto.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
@@ -17,13 +13,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class AuctionController {
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
-    private final BidService bidService;
-
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public MessageResponse greeting(MessageRequest messageRequest) {
+    public MessageResponse greeting(MessageRequest messageRequest) throws InterruptedException {
+        Thread.sleep(1000);
 
-        return new MessageResponse("test",messageRequest.getMessage());
+        MessageResponse response = new MessageResponse("test",messageRequest.getMessage());
+
+        return response;
     }
 }
