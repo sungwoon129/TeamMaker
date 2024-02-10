59bab29ca36244c1b172d58b480b58ad0897262d
master <sungwoony@kakao.com>
2024-02-10 23:39:21 +0900

    fix: 경매상품과 경매상품의 하이라이트 엔티티간의 관계설정이 잘못되어 있는 버그픽스
    - 값타입 -> 일대다 단방향참조관계(경매상품 -> 경매상품의 하이라이트)

--- src/main/java/io/wauction/core/auction/entity/AuctionItem.java
+++ src/main/java/io/wauction/core/auction/entity/AuctionItem.java
@@ -10,13 +10,14 @@ import java.util.List;
 @Table(name = "auction_item")
 public class AuctionItem {
 
+    @Column(name = "item_id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
     @Comment("사진이나 동영상등 경매상품의 가치를 표현할 수 있는 자료")
-    @ElementCollection
-    @CollectionTable(name = "highlight", joinColumns = @JoinColumn(name = "rule_id"))
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "item_id", updatable = false, nullable = false)
     private List<Highlight> highlights = new ArrayList<>();
 }
