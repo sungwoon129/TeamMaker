4750f6451cdf76ab759e929e076c625d8f46f6ed
master <sungwoony@kakao.com>
2024-02-11 00:15:33 +0900

    feat: 운영환경 및 테스트에서 미리 삽입할 데이터(추후 sql문이 아닌 정상적인 루트로 insert 해야함)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,73 @@
+
+
+INSERT INTO `auction_rule` (`rule_id`,`proceed_way`,`skeleton`) VALUES (1,'NORMAL',5);
+
+
+INSERT INTO `participant_role` (`participant_role_id`,`role_name`,`point`,`rule_id`) VALUES (1,'ZEUS','700',1);
+INSERT INTO `participant_role` (`participant_role_id`,`role_name`,`point`,`rule_id`) VALUES (2,'ONER','750',1);
+INSERT INTO `participant_role` (`participant_role_id`,`role_name`,`point`,`rule_id`) VALUES (3,'FAKER','550',1);
+INSERT INTO `participant_role` (`participant_role_id`,`role_name`,`point`,`rule_id`) VALUES (4,'GUMAYUSI','600',1);
+INSERT INTO `participant_role` (`participant_role_id`,`role_name`,`point`,`rule_id`) VALUES (5,'KERIA','650',1);
+
+
+INSERT INTO `team_position` (`position_id`,`position_name`,`rule_id`) VALUES (1,'TOP',1);
+INSERT INTO `team_position` (`position_id`,`position_name`,`rule_id`) VALUES (2,'JUNGLE',1);
+INSERT INTO `team_position` (`position_id`,`position_name`,`rule_id`) VALUES (3,'MID',1);
+INSERT INTO `team_position` (`position_id`,`position_name`,`rule_id`) VALUES (4,'AD CARRY',1);
+INSERT INTO `team_position` (`position_id`,`position_name`,`rule_id`) VALUES (5,'SUPPORT',1);
+
+
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (1,'KIIN',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (2,'CANYON',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (3,'CHOVY',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (4,'PEYZ',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (5,'LEHENDS',1);
+
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (6,'DORAN',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (7,'PEANUT',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (8,'ZEKA',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (9,'VIPER',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (10,'DELIGHT',1);
+
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (11,'PERFECT',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (12,'PYOSIK',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (13,'BDD',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (14,'DEFT',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (15,'BERYL',1);
+
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (16,'KINGEN',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (17,'LUCID',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (18,'SHOWMAKER',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (19,'AIMING',1);
+INSERT INTO `auction_item` (`item_id`,`name`,`rule_id`) VALUES (20,'KELLIN',1);
+
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (1,'KIIN','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',1);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (2,'CANYON','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',2);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (3,'CHOVY','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',3);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (4,'PEYZ','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',4);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (5,'LEHENDS','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',5);
+
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (6,'DORAN','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',6);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (7,'PEANUT','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',7);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (8,'ZEKA','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',8);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (9,'VIPER','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',9);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (10,'DELIGHT','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',10);
+
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (11,'PERFECT','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',11);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (12,'PYOSIK','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',12);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (13,'BDD','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',13);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (14,'DEFT','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',14);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (15,'BERYL','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',15);
+
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (16,'KINGEN','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',16);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (17,'LUCID','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',17);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (18,'SHOWMAKER','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',18);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (19,'AIMING','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',19);
+INSERT INTO `highlight` (`highlight_id`,`name`,`url`,`item_id`) VALUES (20,'KELLIN','https://youtu.be/t1_FVzv4l-4?si=XsPcnLvDt34yg-KK',20);
+
+
+
+
+
+
+
