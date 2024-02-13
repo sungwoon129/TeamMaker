4cd2e2708cdd5bebd3d3a529dfdfe40d2e142db8
master <sungwoony@kakao.com>
2024-02-13 23:56:48 +0900

    refactor: 경매 규칙 정보 응답 dto getter 추가

--- src/main/java/io/wauction/core/auction/dto/AuctionItemResponse.java
+++ src/main/java/io/wauction/core/auction/dto/AuctionItemResponse.java
@@ -2,9 +2,11 @@ package io.wauction.core.auction.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class AuctionItemResponse {
--- src/main/java/io/wauction/core/auction/dto/TeamPositionResponse.java
+++ src/main/java/io/wauction/core/auction/dto/TeamPositionResponse.java
@@ -2,10 +2,13 @@ package io.wauction.core.auction.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class TeamPositionResponse {
     private Long id;
     private String name;
+    private String img;
 }
