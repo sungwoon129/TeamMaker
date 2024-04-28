990e1619b6ef1d501b3d4dc8b0239c7104091b38
master <sungwoony@kakao.com>
2024-04-28 23:39:41 +0900

    fix: mongodb 'AuctionOrder' 엔티티의 id auto increment 효과 적용 2

--- src/main/java/io/wauction/core/auction/entity/AuctionOrder.java
+++ src/main/java/io/wauction/core/auction/entity/AuctionOrder.java
@@ -28,14 +28,14 @@ public class AuctionOrder {
 
     private List<AuctionPlayItem> items;
 
-    protected AuctionOrder(long channelId, List<AuctionPlayItem> items) {
+    protected AuctionOrder(long id, long channelId, List<AuctionPlayItem> items) {
         this.channelId = channelId;
         this.items = items;
     }
 
 
     public static AuctionOrder createAuctionOrder(long id, long channelId, List<AuctionPlayItem> items) {
-        return new AuctionOrder(channelId, items);
+        return new AuctionOrder(id, channelId, items);
     }
 
 }
